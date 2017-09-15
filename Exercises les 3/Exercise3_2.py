b1cacfcec5bb8df758828ae374271f450aff838f
martijn wester <martijn.wester@student.hu.nl>
2017-09-15 16:49:46 +0200

    Exercises les 3 werkboek

---
+++ Exercises les 3/Exercise3_2.py
@@ -0,0 +1,4 @@
+age = int(input('Geef je leeftijd: '))
+paspoort = str(input('Ben je in bezit van een Nederlands paspoort?: '))
+if age >= 18 and paspoort == 'ja':
+    print('Gefeliciteerd!, je mag stemmen')
