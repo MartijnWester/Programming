b38605a1c827194d736ad12724eea85a7bb08e78
martijn wester <martijn.wester@student.hu.nl>
2017-09-15 16:29:32 +0200

    Exercises les 3 werkboek

---
+++ Exercises les 3/Exercise3_1.py
@@ -0,0 +1,7 @@
+score= int(input('Geef je score: '))
+if score > 15:
+    print('Gefeliciteerd!')
+    print('Met een score van', score, 'ben je geslaagd')
+else:
+    print('Helaas')
+    print('Met een score van', score, 'ben je niet geslaagd')
