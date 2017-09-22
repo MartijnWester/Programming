025c01c7236c76de01a4f852cdac9682b1a23240
martijn wester <martijn.wester@student.hu.nl>
2017-09-22 16:47:05 +0200

    Alles van programming map

--- Les 6.py
+++ Les 6.py
@@ -1,14 +1,31 @@
-weight = float(input('Enter your weight in pounds: '))
-height = float(input('Enter your height in inches: '))
-def BMI(weight, height):
-    bmi = (weight*703) / height**2
-    print(bmi)
-    if bmi < 18.5:
-        print("Je hebt ondergewicht")
-    elif bmi > 25.0:
-        print("Je hebt overgewicht")
-    else:
-        print("Je hebt een normaal gewicht")
-
-BMI(weight, height)
-
+# weight = float(input('Enter your weight in pounds: '))
+# height = float(input('Enter your height in inches: '))
+# def BMI(weight, height):
+#     bmi = (weight*703) / height**2
+#     print(int(bmi))
+#     if bmi < 18.5:
+#         print("Je hebt ondergewicht")
+#     elif bmi > 25.0:
+#         print("Je hebt overgewicht")
+#     else:
+#         print("Je hebt een normaal gewicht")
+# BMI(weight, height)
+#
+# pets = ['cat', 'dog', 'fish', 'bird']
+# for i in range(len(pets)):
+#     print(pets[i], end=' ')
+#
+# lst = [3, 2, 7, 1, 9]
+# res=0
+# for num in lst:
+#     res = res + num
+#     print(res)
+#
+# def acronym(phrase):
+#     temp = phrase.split(sep=' ')
+#     print(temp)
+#     acr = ''
+#     for eerste in temp:
+#         acr += eerste[0].upper()
+#     return acr
+# print(acronym(input('Enter the phrase here: ')))
